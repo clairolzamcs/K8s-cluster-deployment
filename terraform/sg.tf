105475768e3bb66acd8d8f7f3bcea8f0f51c210f
clairolzamcs <clairolzamsalazar@gmail.com>
2023-07-01 07:01:56 +0000

    Adding the sg I manually added

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -20,3 +20,15 @@ module "ec2_sg" {
   ingress_rules       = ["ssh-tcp"]
   egress_rules        = ["all-all"]
 }
+
+module "ec2_all" {
+  source  = "terraform-aws-modules/security-group/aws"
+
+  name        = "ec2_allow_all"
+  description = "Security group for ec2_sg"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["all-all"]
+  egress_rules        = ["all-all"]
+}
